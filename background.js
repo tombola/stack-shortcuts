6cdf7748e5d2d355b295bad2654c087cb593e979
tombola <thomas.readings@gmail.com>
2018-03-30 18:07:28 +0100

    Jump to named anchors

--- background.js
+++ background.js
@@ -4,32 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
  console.log("background.js");
 }, false);
 
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.storage.sync.set({snippets: snippets}, function() {
-//     console.log("snippets added to storage.");
-//   });
-//   // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//   //   chrome.declarativeContent.onPageChanged.addRules([{
-//   //     conditions: [new chrome.declarativeContent.PageStateMatcher({
-//   //       pageUrl: {hostEquals: 'projects.torchbox.com'},
-//   //     })
-//   //     ],
-//   //         actions: [new chrome.declarativeContent.ShowPageAction()]
-//   //   }]);
-//   // });
-// });
-
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "skip-up") {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       var current = tabs[0]
-      console.log('skip-up');
+      console.log('skip-up in content script');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {action: "skip-up"}, function(response) {});  
+      });
     });
   }
   if (command == "skip-down") {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       var current = tabs[0]
-      console.log('skip-down');
+      console.log('skip-down in content script');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {action: "skip-down"}, function(response) {});  
+      });
     });
   }
 });
--- scripts/stackoverflow.js
+++ scripts/stackoverflow.js
@@ -1,1 +1,54 @@
-console.log('I have a content script!');
+console.log('I have a content script');
+
+function jump(h){
+    console.log('jump');
+    var url = location.href;
+    location.href = "#"+h;
+    history.replaceState(null,null,url);
+};
+
+function getPostList() {
+  var posts = [];
+  anchors = document.querySelectorAll('a[name]')
+  anchors.forEach(function(el){
+    let post = {};
+    // console.log(el.offsetTop);
+    post = {
+      'offsetTop': el.offsetTop 
+    };
+    posts.push(post);
+  });
+  // console.log(posts);
+  return posts;
+}
+
+function getCurrentPost() {
+  // getPostList();
+}
+
+function getNextPost() {
+  // getCurrentPost();
+}
+
+function getPrevPost() {
+  // getCurrentPost();
+}
+
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+  var posts = [];
+
+  console.log(msg.action);
+  posts = getPostList();
+  console.log(posts);
+
+  
+  switch (msg.action) {
+    case 'skip-up':
+        jump('15195401');
+        break;
+    case 'skip-down':
+        jump('15195401');
+        break;        
+  }
+});
